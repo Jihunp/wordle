b7f5a3aced092cafbd91d07482f2993b2d2d1ecb
Edward Jihun Park <92244135+Jihunp@users.noreply.github.com>
2024-03-21 15:24:17 -0700

    created a new delete functionality

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+
+import React, { useState } from "react";
 import "./Board.css";
 
 const Board = () => {
-  const numRows = 6;
+  const numRows = 1;
   const numCols = 6;
 
   const range = (n) => [...Array(n).keys()];
+  const initialBoxLetters = Array(numRows).fill(Array(numCols).fill(""));
+
+  const [boxContents, setBoxContents] = useState(initialBoxLetters);
+
+  const handleKeyPress = (e, rowIndex, colIndex) => {
+    const newBoxLetters = [...boxContents];
+    const inputChar = e.key.toUpperCase(); // Convert pressed key to uppercase
+    const emptyBoxIndex = newBoxLetters[rowIndex].findIndex(
+      (letter) => letter === ""
+    );
+
+    // Handle Backspace
+    if (inputChar === "BACKSPACE") {
+      newBoxLetters[rowIndex][colIndex] = ""; // Clear the letter inside the box
+      setBoxContents(newBoxLetters);
+      return; // Exit early to prevent further execution
+    }
+
+    if (emptyBoxIndex !== -1) {
+      newBoxLetters[rowIndex][emptyBoxIndex] = inputChar;
+      setBoxContents(newBoxLetters);
+    }
+  };
 
   return (
     <div>
@@ -13,12 +37,19 @@ const Board = () => {
       {range(numRows).map((rowIndex) => (
         <div className="flex-container" key={rowIndex}>
           {range(numCols).map((colIndex) => (
-            <div className="square border-box" key={colIndex}></div>
+            <div
+              className="square border-box"
+              key={colIndex}
+              onKeyDown={(e) => handleKeyPress(e, rowIndex, colIndex)}
+              tabIndex={0}
+            >
+              {boxContents[rowIndex][colIndex]}
+            </div>
           ))}
         </div>
       ))}
     </div>
-  ); 
-}
+  );
+};
 
 export default Board;
